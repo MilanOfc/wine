6c6635cd83705ab7275847d7e1a1f6639b681d83
MilanOfc <antsyrev@gmail.com>
2024-02-02 06:13:54 +0100

    updated README, add argparse for better usage in terminal

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pandas
 import utils
+import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from collections import defaultdict
+
+parser = argparse.ArgumentParser('wine store site\n')
+parser.add_argument('filename', default='wine.xlsx', nargs='?',
+                        help='name of the file, that contains description of wines')
+args = parser.parse_args()
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -12,11 +20,19 @@ template = env.get_template('template.html')
 winery_age = utils.get_age()
 age = utils.get_correct_year_name(winery_age)
 
-drinks = pandas.read_excel('wine3.xlsx', keep_default_na=False)
-drinks_by_category = {}
-for category in drinks['Категория'].unique():
-    drinks_by_category[category] = [drinks.iloc[i].to_dict() for i in range(len(drinks))
-                                    if category in drinks.iloc[i].values]
+eng_column = {
+    'Категория': 'category',
+    'Название': 'name',
+    'Сорт': 'variety',
+    'Цена': 'price',
+    'Картинка': 'picture',
+    'Акция': 'promotion'
+}
+drinks = pandas.read_excel(args.filename, keep_default_na=False).rename(columns=eng_column)
+drinks_by_category = defaultdict(list)
+for drink in drinks.to_dict(orient='records'):
+    drinks_by_category[drink['category']].append(drink)
+
 
 render_page = template.render(
     winery_age=winery_age,
